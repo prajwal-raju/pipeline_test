6f29d0b745a58540bccf400a01b9a632e643e0d9
abpraj <madeinkannada@gmail.com>
2022-07-18 22:55:00 +0530

    First Commit

---
+++ src/HelloWorldTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    
+    private HelloWorld subject;
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+	
+
+}
